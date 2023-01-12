98f7fdfa9a5a3849ef866c32041102441e946c21
Lucas Numeriano <116529579+Elasukou@users.noreply.github.com>
2023-01-12 14:43:29 -0300

    atualização ex010

--- ex010/script.js
+++ ex010/script.js
@@ -1,15 +1,12 @@
-let pergunta = window.prompt("Você sabe quais anos tivemos Copa? Responda 1 para sim ou 2 para não")
-
-let S = 1;
-let N = 2;
-
-if (pergunta == S) {
-    alert("OK!")
-} else {
-    let anoCopa = 1930;
-
-    while(anoCopa <= 2024) {
-        res.innerHTML = "Tivemos Copa em " + anoCopa
-        anoCopa = anoCopa + 4;
-    }
+let anoCopa = 1930
+while (anoCopa <= 2022) {
+    if (anoCopa == 1942) {
+        console.log('A Copa do mundo de 1942 foi cancelada por conta da Segunda Grande Guerra')
+        anoCopa = anoCopa + 4
+    } else if (anoCopa == 1946) {
+        console.log('A Copa do mundo de 1946 foi cancelada por conta da Segunda Grande Guerra')
+        anoCopa = anoCopa + 4
+    } else {
+    console.log(`Tivemos copa em ${anoCopa}`)
+    anoCopa = anoCopa + 4 }
 }
